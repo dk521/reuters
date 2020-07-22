cae18b73fe5bef688d2830c4f4fb515f584acfaf
nexuspro47 <nexuspro47@gmail.com>
2020-07-22 15:25:18 +0530

    first scrap URLS then scrap Content

--- crawlers/crawlers/spiders/article.py
+++ crawlers/crawlers/spiders/article.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import pandas as pd
+
 
 
 class ArticleSpider(scrapy.Spider):
@@ -7,12 +9,27 @@ class ArticleSpider(scrapy.Spider):
     allowed_domains = ['in.reuters.com']
 
     # Replace this for new links to be crawled
-    start_urls = ['http://in.reuters.com/article/us-china-economy-lpr/china-keeps-lending-benchmark-lpr-steady-for-third-month-as-expected-idINKCN24L04R']
+    # start_urls = ['http://in.reuters.com/article/us-china-economy-lpr/china-keeps-lending-benchmark-lpr-steady-for-third-month-as-expected-idINKCN24L04R'] 
+        
+
+    def start_requests(self):
+
+        df = pd.read_csv('output.csv')        
+        # print(df.url.head())
+        urls = []
+        for i in df.url:
+            urls.append('http://in.reuters.com'+str(i))
+        
+        # for i in urls:
+        #     print(i,"\n\n")
+
+        start_urls = reversed( urls )
+        return [ scrapy.Request(url = start_url) for start_url in start_urls ]
+
+
 
     def parse(self, response):
-        
         for article in response.xpath("//div[@class='StandardArticle_inner-container']"):
-
             content_list = []
             for i in response.xpath("//div[@class='StandardArticleBody_body']/p"):
                 #print(i.xpath('string()').extract()[0])
@@ -20,11 +37,11 @@ class ArticleSpider(scrapy.Spider):
                 content_list.append(i.xpath('string()').extract()[0])
             
             content = ' '.join(content_list)
-
             yield {
                 'title' : article.xpath(".//h1/text()").get(),
                 'content' : content,
                 'author_editor' : article.xpath(".//div[@class='Attribution_attribution']/p/text()").get()
             }
+        
 
 
--- crawlers/crawlers/spiders/reuters.py
+++ crawlers/crawlers/spiders/reuters.py
@@ -2,9 +2,9 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from .article import ArticleSpider
+from time import sleep
 
-
-class ReutersSpider(scrapy.Spider):
+class ReutersSpider(scrapy.Spider): 
     name = 'reuters'
     allowed_domains = ['in.reuters.com']
 
@@ -18,19 +18,18 @@ class ReutersSpider(scrapy.Spider):
         for article in response.xpath("//div[@class='news-headline-list  ']/article[@class='story ']"):
 
             print("\n\nNew Article")        
-            yield {
+            yield ({
                 # mention the xpath for the element to be captured.
                 'title' : article.xpath(".//div[@class='story-content']/a/h3/text()").get(),
                 'url' : article.xpath(".//div[@class='story-content']/a/@href").get(),
                 'press_release' : article.xpath(".//div[@class='story-content']/time[@class='article-time']/span/text()").get(),
-
-            }
+            })  
             
-            article_url = response.urljoin(article.xpath(".//div[@class='story-content']/a/@href").get())
-            yield scrapy.Request(url = article_url, callback = self.parse_article)
+            # article_url = response.urljoin(article.xpath(".//div[@class='story-content']/a/@href").get())
+            # print("Article Url: ", article_url, "\n\n")
+            # return scrapy.Request(url = article_url, callback = self.parse_article, priority=1)
 
         next_page_link = response.xpath("//a[@class='control-nav-next']/@href").get()
-
         if next_page_link:
             
             # yield response.follow(url = next_page_link, callback = self.NewswireSpider)
@@ -55,5 +54,92 @@ class ReutersSpider(scrapy.Spider):
                 'content' : content,
                 'author_editor' : article.xpath(".//div[@class='Attribution_attribution']/p/text()").get()
             }
+            print("\n<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class ReutersSpider(scrapy.Spider):
+#     name = 'reuters'
+#     allowed_domains = ['in.reuters.com']
+
+#     # here, list down the URLS for dynamic Scrapping
+#     start_urls = ['https://in.reuters.com/news/archive/rates-rss']
+
+
+#     def parse(self, response):
+    
+
+#         for article in response.xpath("//div[@class='news-headline-list  ']/article[@class='story ']"):
+
+#             print("\n\nNew Article")        
+#             yield {
+#                 # mention the xpath for the element to be captured.
+#                 'title' : article.xpath(".//div[@class='story-content']/a/h3/text()").get(),
+#                 'url' : article.xpath(".//div[@class='story-content']/a/@href").get(),
+#                 'press_release' : article.xpath(".//div[@class='story-content']/time[@class='article-time']/span/text()").get(),
+#             }
+            
+#             article_url = response.urljoin(article.xpath(".//div[@class='story-content']/a/@href").get())
+#             print("Article Url: ", article_url, "\n\n")
+#             yield scrapy.Request(url = article_url, callback = self.parse_article, priority=1)
+
+#         next_page_link = response.xpath("//a[@class='control-nav-next']/@href").get()
+
+#         # sleep(5)
+#         if next_page_link:
+            
+#             # yield response.follow(url = next_page_link, callback = self.NewswireSpider)
+#             absolute_url = response.urljoin(next_page_link)
+            
+#             yield scrapy.Request(url = absolute_url, callback = self.parse)
+        
+
+            
+#     def parse_article(self, response):
+        
+#         for article in response.xpath("//div[@class='StandardArticle_inner-container']"):
+
+#             content_list = []
+#             for i in response.xpath("//div[@class='StandardArticleBody_body']/p"):
+#                 content_list.append(i.xpath('string()').extract()[0])
+            
+#             content = ' '.join(content_list)
+            
+#             yield {
+#                 'title' : article.xpath(".//h1/text()").get(),
+#                 'content' : content,
+#                 'author_editor' : article.xpath(".//div[@class='Attribution_attribution']/p/text()").get()
+#             }
+#             print("\n<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n")
 
 
